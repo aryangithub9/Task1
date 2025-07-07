73c2100e44c6b9dfd1b951683ac3ebaf38f0ccb2
Aryan <aryansinghdhampur@gmail.com>
2025-07-07 22:04:59 +0530

    db

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import connectDB from './config/db.js';
+import connectDB from './db.js';
 import adTagRoutes from './routes/AdtagRoutes.js';
 
 dotenv.config();
