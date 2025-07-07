1596ce47e10d42890334b165933ec07d342c5a7a
Aryan <aryansinghdhampur@gmail.com>
2025-07-07 11:08:17 +0530

    add tracker for pages

---
+++ frontend/src/AnalyticsTracker.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const AnalyticsTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!window.gtag) return;
+
+    window.gtag('event', 'page_view', {
+      page_path: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+};
+
+export default AnalyticsTracker;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import File from './File'
 import Navigation from './Navigation'
+import AnalyticsTracker from './AnalyticsTracker'
 
 function App() {
   return (
    <>
+   <AnalyticsTracker>
    <div className='flex justify-center items-center'><Navigation/></div>
    <div className='text-center text-2xl'><File/></div>
+   </AnalyticsTracker>
    
    </>
   )
