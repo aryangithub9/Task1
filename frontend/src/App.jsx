8a5432abf8e1fe8546c3d55149d2d093a56f04ff
Aryan <aryansinghdhampur@gmail.com>
2025-07-07 11:22:18 +0530

    fix some errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import File from './File'
-import Navigation from './Navigation'
-import AnalyticsTracker from './AnalyticsTracker'
+import React from 'react';
+import File from './File';
+import Navigation from './Navigation';
+import AnalyticsTracker from './AnalyticsTracker';
 
 function App() {
   return (
-   <>
-   <AnalyticsTracker>
-   <div className='flex justify-center items-center'><Navigation/></div>
-   <div className='text-center text-2xl'><File/></div>
-   </AnalyticsTracker>
-   
-   </>
-  )
+    <>
+      <AnalyticsTracker /> 
+      <div className="flex justify-center items-center">
+        <Navigation />
+      </div>
+      <div className="text-center text-2xl">
+        <File />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
